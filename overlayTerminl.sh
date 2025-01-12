1099f3c6ed2ac0cdea6123c610a03ecaa4f7fc28
jothish babu <jothishwp8@outlook.com>
2025-01-12 22:30:41 +0530

    added swaybar script

--- overlayTerminl.sh
+++ overlayTerminl.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
 # Terminal class name
-TERMINAL_CLASS="KittyOverlay"  # Change this based on your terminal class
+TERMINAL_CLASS='KittyOverlay'  # Change this based on your terminal class
 
 # Check if the terminal is already running
-WIN_ID=$(hyprctl clients | awk -v class='KittyOverlay' '
+WIN_ID=$(hyprctl clients | awk -v class=$TERMINAL_CLASS '
       /Window/ {win_id=$2}  # Capture the window ID
       /class:/ && $2 == class {print win_id}  # Match the class and print the saved ID
       ')
 
-if [ -z "$WIN_ID" ]; then
-  # Terminal is not running, launch it
-  kitty --class $TERMINAL_CLASS &
-else
-  # Terminal is running, toggle its visibility
+# Check if a window ID was found
+if [ -n "$WIN_ID" ]; then
+  # Window ID found, toggle the special workspace
   hyprctl dispatch togglespecialworkspace $WIN_ID
+else
+  # No matching window found, launch the terminal
+  kitty --class "$TERMINAL_CLASS" &
 fi

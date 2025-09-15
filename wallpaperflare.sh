18d6ee5dc0974d1ea1de71606bb1dd5f48f4cf15
jothish babu <jothishwp8@outlook.com>
2025-09-15 23:57:17 +0530

    updated the wallpaper script with local support

--- wallpaperflare.sh
+++ wallpaperflare.sh
@@ -1,75 +1,82 @@
 #!/bin/bash
 
+#logging
+logfile=/tmp/wallpaperflare.log
+exec > >(tee -a "$logfile") 2>&1
+
 urls=()
 urlLength=0
 randomNo=0
+websiteLiveStatusCode=0
 
-AccessCookie="cf_clearance=Gmhw3H0RAHTmyWqmsi_HeWtlXxzKlZAj4WGIhapPUDk-1752823068-1.2.1.1-WJzKS3J7cFxs5lzv8JlvEbusgvnpaoUhwWrXMf9.bEQxzgS7siwHZ7c3tPaHmhCS8ASKoHf3pqiF_ZMQyqMc.bVDEF5yDXJXdp4xOfjcI2sBZa6zZm7ozFtU6VJXwtTWzYMjCDHpJVgLoNmxrNh.DjSMB2F5Yo9ZBiy4jL4c9meHwIRb4i6TJtMWcR9Z_fM0FXjBYCMDnWOzdpcJFk3v5m2v3biBGbBE7YYwwQkaO88"
-test(){
-    curlHeader=" --compressed \
-            -H 'User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:140.0) Gecko/20100101 Firefox/140.0' \
-            -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8' \
-            -H 'Accept-Language: en-GB' \
-            -H 'Accept-Encoding: gzip, deflate, br, zstd' \
-            -H 'Connection: keep-alive' \
-            -H 'Cookie: $AccessCookie ' \
-            -H 'Upgrade-Insecure-Requests: 1' \
-            -H 'Sec-Fetch-Dest: document' \
-            -H 'Sec-Fetch-Mode: navigate' \
-            -H 'Sec-Fetch-Site: cross-site' \
-            -H 'Priority: u=0, i' \
-            -H 'Pragma: no-cache' \
-            -H 'Cache-Control: no-cache'" 
-    eval "curl -sS https://www.wallpaperflare.com/ " $curlHeader
+#Local wallpaper directory
+localWallpaperDir=~/Wallpapers/
+
+AccessCookie="cf_clearance=tCezNZayZqhpReWYSses87WFaCJKLXOOVpSGmR7LLas-1757959726-1.2.1.1-Jq9OEttUoFlxtk_CiGVw_fyHaSnJRRcVC4U8N3zku43nxKFOQALhMFbMN_blZ8OxdwT0LZVyx6ewMPMYugrYYn5cJKXLEFkOUo3QEx1KtqhzoaEUqIa8bch9q14p4UVIOgHdXo_7xbsAi0be21ou4Yer1wHiZ9xu_93jD_sHAWjtyJo75EK1sdxUdHvCwMuxdfowVbQtAQAawfAYtwikPbwXmJHtQU6quQkK6dQYhLA"
+
+curlHeader=(
+  --compressed
+  -H 'User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:142.0) Gecko/20100101 Firefox/142.0'
+  -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8'
+  -H 'Accept-Language: en-GB'
+  -H 'Accept-Encoding: gzip, deflate, br, zstd'
+  -H 'Alt-Used: www.wallpaperflare.com'
+  -H 'Connection: keep-alive'
+  -H "Cookie: $AccessCookie"
+  -H 'Upgrade-Insecure-Requests: 1'
+  -H 'Sec-Fetch-Dest: document'
+  -H 'Sec-Fetch-Mode: navigate'
+  -H 'Sec-Fetch-Site: cross-site'
+  -H 'Priority: u=0, i'
+  -H 'TE: trailers'
+)
+
+#when the website is down or not accessible
+function updateLocalFileAsWall(){
+    echo "Updating local file as wallpaper"
+    #get Random file from wallpaper directory
+    selectedFile=$localWallpaperDir$(ls $localWallpaperDir | shuf -n 1)
+
+    # updating the wallpaper
+    swaymsg "output * bg $selectedFile fill" > /dev/null
 }
 
+#website accessible check
+function websitePing(){
+    echo check if website is accessible
 
-updateUrls(){
+    websiteLiveStatusCode=$(curl -s -o /dev/null -w "%{http_code}\n"  'https://www.wallpaperflare.com/' "${curlHeader[@]}")
 
-    curlHeader=" --compressed \
-                -H 'User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:140.0) Gecko/20100101 Firefox/140.0' \
-                -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8' \
-                -H 'Accept-Language: en-GB' \
-                -H 'Accept-Encoding: gzip, deflate, br, zstd' \
-                -H 'Connection: keep-alive' \
-                -H 'Cookie: $AccessCookie ' \
-                -H 'Upgrade-Insecure-Requests: 1' \
-                -H 'Sec-Fetch-Dest: document' \
-                -H 'Sec-Fetch-Mode: navigate' \
-                -H 'Sec-Fetch-Site: cross-site' \
-                -H 'Priority: u=0, i' \
-                -H 'Pragma: no-cache' \
-                -H 'Cache-Control: no-cache'" 
+    #caling the get website detals func
+    updateUrls
+    #getting the length of urls
+    urlLength=${#urls[@]}
+    echo $urlLength found this much wallpaper
+}
 
+function updateUrls(){
+    echo grabbing the wallpapers urls
     #loding urls to array
-    mapfile -t urls < <( eval "curl -sS https://www.wallpaperflare.com/ " $curlHeader | grep -Eo '(https://www\.wallpaperflare\.com/.+-[^"]+)')
-    
+    mapfile -t urls < <( curl -sS "https://www.wallpaperflare.com/" "${curlHeader[@]}" | grep -Eo '(https://www\.wallpaperflare\.com/.+-[^"]+)')
 }
-
-#caling the get website detals func
-updateUrls
-
-#getting the length of urls 
-urlLength=${#urls[@]}
-
-
-echo $urlLength found this much wallpaper >> /tmp/wallpaperflare.log
 
 function updateWall {
+
+    echo updating the wallpapers from website
 
     #getting a random no between 1- length of the array
     randomNo=$(shuf -i 1-$urlLength -n 1)
 
-    echo $randomNo selected random no >> /tmp/wallpaperflare.log
+    echo $randomNo selected random no
 
     #getting the wallpaper fullsize url
 
-    wallpaper=$( eval "curl -s ${urls[$randomNo]}/download" $curlHeader  | grep -Eo '(https://r4\.wallpaperflare\.com/.+-[^"]+)')
+    wallpaper=$( curl -s "${urls[$randomNo]}/download" "${curlHeader[@]}"  | grep -Eo '(https://r4\.wallpaperflare\.com/.+-[^"]+)')
 
-    echo $wallpaper selected wallpaper url >> /tmp/wallpaperflare.log
+    echo $wallpaper selected wallpaper url
 
     if [[ -z $wallpaper ]];then
-        echo "wallpaper not found" >> /tmp/wallpaperflare.log
+        echo "wallpaper not found"
         return
     fi
 
@@ -81,12 +88,27 @@ function updateWall {
 
     # updating the wallpaper
     swaymsg "output * bg /tmp/wall.jpg fill" > /dev/null
-
 }
+
+#check the website readness
+websitePing
 
 #infinite loop that will upddate and downoad urls
 while true;do
-    #updating function call
-    updateWall;
+
+    #check website readness
+    if [[ $websiteLiveStatusCode -eq 200 ]];then
+        #updating function call
+        updateWall;
+
+    else
+        echo website not accessible
+        #checking again for website readness
+        websitePing
+
+        #seting local file as wallpaper
+        updateLocalFileAsWall
+    fi
+
     sleep 60;
 done
